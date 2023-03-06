dc59dd58ee4b7e49e58d9fd37b666fed8b5358c5
류원창 <wom05169@gmail.com>
2023-03-06 17:49:50 +0900

    노마드코더 웹툰 앱 실습 중

---
+++ 04_nomard_flutter/lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          '오늘의 웹툰',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 2,
+      ),
+    );
+  }
+}
